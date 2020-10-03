945431a0b7907b6fc1f7601a19970979b7679719
Vijay <vijaythevirtue@gmail.com>
2020-10-03 15:21:09 +0530

    Scan QR | Build Passing

--- lib/QR Scanner/ScanQR.dart
+++ lib/QR Scanner/ScanQR.dart
@@ -1,4 +1,6 @@
+import 'package:barcode_scan/barcode_scan.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ScanQR extends StatefulWidget {
   ScanQR({Key key}) : super(key: key);
@@ -6,6 +8,10 @@ class ScanQR extends StatefulWidget {
   @override
   _ScanQRState createState() => _ScanQRState();
 }
+
+String qrData = "No data found!";
+var data;
+bool hasdata = false;
 
 class _ScanQRState extends State<ScanQR> {
   @override
@@ -16,8 +22,69 @@ class _ScanQRState extends State<ScanQR> {
         appBar: AppBar(
           title: Text("QR Scanner"),
         ),
-        body: Center(
-          child: Text("QR"),
+        body: Container(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Flexible(
+                    child: Text(
+                      "Raw Data:  ${(qrData)}",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.launch_outlined),
+                    onPressed: hasdata
+                        ? () async {
+                            if (await canLaunch(qrData)) {
+                              print(qrData);
+                              await launch(qrData);
+                            } else {
+                              throw 'Could not launch ';
+                            }
+                          }
+                        : null,
+                  ),
+                ],
+              ),
+              SizedBox(height: 15),
+              Container(
+                width: ((MediaQuery.of(context).size.width) / 2) - 45,
+                height: 35,
+                child: OutlineButton(
+                  focusColor: Colors.red,
+                  highlightColor: Colors.blue,
+                  hoverColor: Colors.lightBlue[100],
+                  splashColor: Colors.blue,
+                  borderSide: BorderSide(
+                    width: 3,
+                    color: Colors.blue,
+                  ),
+                  shape: StadiumBorder(),
+                  child: Text(
+                    "Scan QR",
+                    style: TextStyle(fontSize: 17),
+                  ),
+                  onPressed: () async {
+                    var options = ScanOptions(
+                      autoEnableFlash: true,
+                    );
+                    var data = await BarcodeScanner.scan(options: options);
+                    setState(() {
+                      qrData = data.rawContent.toString();
+                      hasdata = true;
+                    });
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
